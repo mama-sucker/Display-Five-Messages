75e524706f012f0ab55520e719364a2aaef17aef
Addison Roy <74374312+mama-sucker@users.noreply.github.com>
2024-08-29 23:45:16 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+// Module 1: Excercise 1.2 
+// 1.2 (Display five messages) Write a program that displays Welcome to Java five times.
+// By Addison Roy
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Repeat Phrase Five times
+        int repeatPhrase = 5; 
+        for (int i = 0; i < repeatPhrase; i++) {
+            System.out.println("Welcome to Java!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+
+COP-2800 Module 1: Excercise 1.1
+
+Module 1: Excercise 1.2 
+1.2 (Display five messages) Write a program that displays Welcome to Java five times.
+By Addison Roy
